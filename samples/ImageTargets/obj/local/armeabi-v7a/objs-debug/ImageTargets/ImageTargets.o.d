a00838b415e6303c23f088cbd15d2998ccd34bdd
Erin Jamroz <erinjamroz@gmail.com>
2012-10-22 16:07:42 -0700

    updated

--- samples/ImageTargets/obj/local/armeabi-v7a/objs-debug/ImageTargets/ImageTargets.o.d
+++ samples/ImageTargets/obj/local/armeabi-v7a/objs-debug/ImageTargets/ImageTargets.o.d
@@ -1,30 +1,30 @@
 obj/local/armeabi-v7a/objs-debug/ImageTargets/ImageTargets.o: \
  jni/ImageTargets.cpp \
- /Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/jni.h \
- /Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/android/log.h \
- /Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/stdio.h \
- /Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/sys/cdefs.h \
- /Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h \
- /Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/android/api-level.h \
- /Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/sys/_types.h \
- /Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/machine/_types.h \
- /Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/sys/types.h \
- /Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/stdint.h \
- /Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/linux/posix_types.h \
- /Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/linux/stddef.h \
- /Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/linux/compiler.h \
- /Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/asm/posix_types.h \
- /Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/asm/types.h \
- /Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/linux/types.h \
- /Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/machine/kernel.h \
- /Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h \
- /Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/string.h \
- /Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/malloc.h \
- /Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/assert.h \
- /Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/GLES2/gl2.h \
- /Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/GLES2/gl2platform.h \
- /Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/KHR/khrplatform.h \
- /Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/GLES2/gl2ext.h \
+ /Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/jni.h \
+ /Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/android/log.h \
+ /Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/stdio.h \
+ /Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/sys/cdefs.h \
+ /Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/android/api-level.h \
+ /Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/sys/_types.h \
+ /Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/machine/_types.h \
+ /Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/sys/types.h \
+ /Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/stdint.h \
+ /Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/linux/posix_types.h \
+ /Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/linux/stddef.h \
+ /Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/linux/compiler.h \
+ /Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/asm/posix_types.h \
+ /Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/asm/types.h \
+ /Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/linux/types.h \
+ /Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/machine/kernel.h \
+ /Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h \
+ /Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/string.h \
+ /Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/malloc.h \
+ /Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/assert.h \
+ /Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/GLES2/gl2.h \
+ /Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/GLES2/gl2platform.h \
+ /Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/KHR/khrplatform.h \
+ /Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/GLES2/gl2ext.h \
  jni/../../../build/include/QCAR/QCAR.h \
  jni/../../../build/include/QCAR/System.h \
  jni/../../../build/include/QCAR/CameraDevice.h \
@@ -46,55 +46,55 @@ obj/local/armeabi-v7a/objs-debug/ImageTargets/ImageTargets.o: \
  jni/../../../build/include/QCAR/DataSet.h jni/SampleUtils.h \
  jni/Texture.h jni/CubeShaders.h jni/Teapot.h jni/banana.h
 
-/Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/jni.h:
+/Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/jni.h:
 
-/Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/android/log.h:
+/Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/android/log.h:
 
-/Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/stdio.h:
+/Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/stdio.h:
 
-/Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/sys/cdefs.h:
+/Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/sys/cdefs.h:
 
-/Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h:
+/Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h:
 
-/Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/android/api-level.h:
+/Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/android/api-level.h:
 
-/Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/sys/_types.h:
+/Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/sys/_types.h:
 
-/Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/machine/_types.h:
+/Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/machine/_types.h:
 
-/Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/sys/types.h:
+/Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/sys/types.h:
 
-/Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/stdint.h:
+/Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/stdint.h:
 
-/Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/linux/posix_types.h:
+/Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/linux/posix_types.h:
 
-/Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/linux/stddef.h:
+/Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/linux/stddef.h:
 
-/Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/linux/compiler.h:
+/Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/linux/compiler.h:
 
-/Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/asm/posix_types.h:
+/Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/asm/posix_types.h:
 
-/Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/asm/types.h:
+/Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/asm/types.h:
 
-/Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/linux/types.h:
+/Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/linux/types.h:
 
-/Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/machine/kernel.h:
+/Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/machine/kernel.h:
 
-/Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h:
+/Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h:
 
-/Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/string.h:
+/Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/string.h:
 
-/Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/malloc.h:
+/Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/malloc.h:
 
-/Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/assert.h:
+/Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/assert.h:
 
-/Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/GLES2/gl2.h:
+/Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/GLES2/gl2.h:
 
-/Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/GLES2/gl2platform.h:
+/Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/GLES2/gl2platform.h:
 
-/Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/KHR/khrplatform.h:
+/Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/KHR/khrplatform.h:
 
-/Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/GLES2/gl2ext.h:
+/Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/GLES2/gl2ext.h:
 
 jni/../../../build/include/QCAR/QCAR.h:
 
--- samples/ImageTargets/obj/local/armeabi/objs-debug/ImageTargets/SampleUtils.o.d
+++ samples/ImageTargets/obj/local/armeabi/objs-debug/ImageTargets/SampleUtils.o.d
@@ -1,106 +1,106 @@
 obj/local/armeabi/objs-debug/ImageTargets/SampleUtils.o: \
  jni/SampleUtils.cpp jni/SampleUtils.h \
- /Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/stdio.h \
- /Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/sys/cdefs.h \
- /Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h \
- /Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/android/api-level.h \
- /Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/sys/_types.h \
- /Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/machine/_types.h \
- /Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/sys/types.h \
- /Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/stdint.h \
- /Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/linux/posix_types.h \
- /Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/linux/stddef.h \
- /Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/linux/compiler.h \
- /Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/asm/posix_types.h \
- /Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/asm/types.h \
- /Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/linux/types.h \
- /Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/machine/kernel.h \
- /Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h \
- /Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/android/log.h \
- /Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/math.h \
- /Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/limits.h \
- /Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/sys/limits.h \
- /Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/linux/limits.h \
- /Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/machine/internal_types.h \
- /Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/machine/limits.h \
- /Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/sys/syslimits.h \
- /Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/stdlib.h \
- /Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/string.h \
- /Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/malloc.h \
- /Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/alloca.h \
- /Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/strings.h \
- /Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/memory.h \
- /Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/GLES2/gl2.h \
- /Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/GLES2/gl2platform.h \
- /Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/KHR/khrplatform.h \
- /Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/GLES2/gl2ext.h
+ /Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/stdio.h \
+ /Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/sys/cdefs.h \
+ /Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/android/api-level.h \
+ /Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/sys/_types.h \
+ /Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/machine/_types.h \
+ /Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/sys/types.h \
+ /Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/stdint.h \
+ /Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/linux/posix_types.h \
+ /Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/linux/stddef.h \
+ /Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/linux/compiler.h \
+ /Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/asm/posix_types.h \
+ /Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/asm/types.h \
+ /Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/linux/types.h \
+ /Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/machine/kernel.h \
+ /Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h \
+ /Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/android/log.h \
+ /Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/math.h \
+ /Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/limits.h \
+ /Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/sys/limits.h \
+ /Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/linux/limits.h \
+ /Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/machine/internal_types.h \
+ /Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/machine/limits.h \
+ /Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/sys/syslimits.h \
+ /Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/stdlib.h \
+ /Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/string.h \
+ /Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/malloc.h \
+ /Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/alloca.h \
+ /Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/strings.h \
+ /Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/memory.h \
+ /Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/GLES2/gl2.h \
+ /Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/GLES2/gl2platform.h \
+ /Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/KHR/khrplatform.h \
+ /Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/GLES2/gl2ext.h
 
 jni/SampleUtils.h:
 
-/Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/stdio.h:
+/Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/stdio.h:
 
-/Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/sys/cdefs.h:
+/Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/sys/cdefs.h:
 
-/Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h:
+/Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h:
 
-/Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/android/api-level.h:
+/Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/android/api-level.h:
 
-/Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/sys/_types.h:
+/Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/sys/_types.h:
 
-/Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/machine/_types.h:
+/Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/machine/_types.h:
 
-/Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/sys/types.h:
+/Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/sys/types.h:
 
-/Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/stdint.h:
+/Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/stdint.h:
 
-/Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/linux/posix_types.h:
+/Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/linux/posix_types.h:
 
-/Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/linux/stddef.h:
+/Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/linux/stddef.h:
 
-/Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/linux/compiler.h:
+/Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/linux/compiler.h:
 
-/Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/asm/posix_types.h:
+/Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/asm/posix_types.h:
 
-/Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/asm/types.h:
+/Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/asm/types.h:
 
-/Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/linux/types.h:
+/Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/linux/types.h:
 
-/Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/machine/kernel.h:
+/Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/machine/kernel.h:
 
-/Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h:
+/Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h:
 
-/Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/android/log.h:
+/Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/android/log.h:
 
-/Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/math.h:
+/Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/math.h:
 
-/Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/limits.h:
+/Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/limits.h:
 
-/Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/sys/limits.h:
+/Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/sys/limits.h:
 
-/Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/linux/limits.h:
+/Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/linux/limits.h:
 
-/Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/machine/internal_types.h:
+/Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/machine/internal_types.h:
 
-/Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/machine/limits.h:
+/Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/machine/limits.h:
 
-/Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/sys/syslimits.h:
+/Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/sys/syslimits.h:
 
-/Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/stdlib.h:
+/Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/stdlib.h:
 
-/Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/string.h:
+/Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/string.h:
 
-/Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/malloc.h:
+/Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/malloc.h:
 
-/Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/alloca.h:
+/Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/alloca.h:
 
-/Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/strings.h:
+/Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/strings.h:
 
-/Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/memory.h:
+/Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/memory.h:
 
-/Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/GLES2/gl2.h:
+/Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/GLES2/gl2.h:
 
-/Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/GLES2/gl2platform.h:
+/Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/GLES2/gl2platform.h:
 
-/Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/KHR/khrplatform.h:
+/Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/KHR/khrplatform.h:
 
-/Users/matthewcburke/dev/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/GLES2/gl2ext.h:
+/Development/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/GLES2/gl2ext.h:
