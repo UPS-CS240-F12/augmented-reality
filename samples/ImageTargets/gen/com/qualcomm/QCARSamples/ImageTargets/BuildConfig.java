d1109d5dd3ee6df5a9206a19940922a5c9e858ce
Matt Burke <matthewcburke@gmail.com>
2012-10-31 15:51:10 -0700

    Removing files now ignored by .gitignore.

--- samples/ImageTargets/gen/com/qualcomm/QCARSamples/ImageTargets/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.qualcomm.QCARSamples.ImageTargets;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
